7c5d7af06674611d9725284e5ea3645d6e77ce02
ican243 <ican243@gmail.com>
2026-04-23 16:52:43 +0900

    js study

---
+++ circle.js
@@ -0,0 +1,9 @@
+const PI = 3.141592;
+function getArea(redius) {
+    return PI * radius * radius;
+}
+function getCircumference(radius) {
+    return 2 * PI * radius;
+}
+
+export { PI, getArea, getCircumference };
